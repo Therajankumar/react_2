d3a1cc2f4f087bb6b55b7b82c53c9cabc6aa7b00
Therajankumar <kumarrajanthakur2005@gmail.com>
2025-04-26 14:51:37 +0530

    episode-01

---
+++ APP.js
@@ -0,0 +1,29 @@
+/*<div id = "parent">
+*      <div id = "child">
+*         <h1>I am an h1 tag</
+*     </div>
+*
+*React.createElement(object)=>HTML(BROWSER UNDERSTANDS)
+*/
+
+
+const parent =React.createElement(
+    "div",
+     {id:"parent"},
+    React.createElement(
+        "div",
+         {id:"child"},
+         React.createElement(
+            "h1",
+            {},
+            "I am an h1 tag")
+            
+         )
+        );
+
+console.log(parent);//object
+
+
+const root =ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
